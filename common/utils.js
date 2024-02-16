653eb6fed4cb5f5769635b1ddd9b8ca82467a58d
nirmal-techp <nirmal@techprescient.com>
2024-02-16 18:42:38 +0530

    header with menuitem toggle

--- common/utils.js
+++ common/utils.js
@@ -3,17 +3,13 @@ function loadModalEventHandlers() {
   const menuIcon = document.getElementById("menu-icon");
   const menu = document.getElementById("menu");
   const menuItems = document.querySelectorAll('.menu-item');
-  const menuClose = document.getElementById("menu-close");
+
   document
     .getElementById("menuItemHeaderJoinInterest")
     .addEventListener("click", openModal);
 
   menuIcon.addEventListener("click", function () {
     menu.classList.toggle("show");
-    toggleMenuIcon();
-  });
-  menuClose.addEventListener("click", function () {
-    menu.classList.remove("show");
     toggleMenuIcon();
   });
 
@@ -26,12 +22,7 @@ function loadModalEventHandlers() {
   });
 
   function toggleMenuIcon() {
-    menuIcon.style.display = menu.classList.contains("show")
-      ? "none"
-      : "block";
-    menuClose.style.display = menu.classList.contains("show")
-      ? "block"
-      : "none";
+    menuIcon.classList.toggle("change");
   }
 
   // Close menu when clicking outside
@@ -42,7 +33,7 @@ function loadModalEventHandlers() {
     ) {
       menu.classList.remove("show");
       menuIcon.style.display = "block";
-      menuClose.style.display = "none";
+      document.getElementById("menu-icon").classList.remove("change")
     }
   });
 }
